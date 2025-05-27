de0f8af7d2c0be6cdb373f0feb0bf4380e40a4a9
Skyler688 <skylergit86@gmail.com>
2025-05-27 09:21:01 -0600

    Initial commit

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="left"></div>
+        <div className="center"></div>
+        <div className="right"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
