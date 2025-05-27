112509af4abdad5df90f4ecfcb75766c95c7eca5
Skyler688 <skylergit86@gmail.com>
2025-05-27 10:57:12 -0600

    header and footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,7 +1,57 @@
 import React from "react";
+import "./Footer.scss";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <div className="top">
+        <div className="item">
+          <h1>Categories</h1>
+          <span>Women</span>
+          <span>Men</span>
+          <span>Shoes</span>
+          <span>Accessories</span>
+          <span>New Arrivals</span>
+        </div>
+        <div className="item">
+          <h1>Links</h1>
+          <span>FAQ</span>
+          <span>Pages</span>
+          <span>Stores</span>
+          <span>Compare</span>
+          <span>Cookies</span>
+        </div>
+        <div className="item">
+          <h1>About</h1>
+          <span>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam
+            harum hic ipsam dolorum! Aliquam ut nisi aperiam nemo eum laudantium
+            quis in quia numquam? Ducimus id illum quas nostrum maxime.
+          </span>
+        </div>
+        <div className="item">
+          <h1>Contacts</h1>
+          <span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae ipsa,
+            sit quo libero quidem, maxime eos placeat maiores assumenda, fugit
+            velit facilis voluptates exercitationem dolorum hic ullam expedita?
+            Ipsum, error?
+          </span>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="left">
+          <span className="logo">Lamastore</span>
+          <span className="copyright">
+            © copyright 2023. All Rights Reserved
+          </span>
+        </div>
+        <div className="right">
+          <img src="/img/payment.png" alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
