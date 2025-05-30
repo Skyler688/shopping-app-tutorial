b447a70f14f811978e3034d9076be7d3ecfec288
Skyler688 <skylergit86@gmail.com>
2025-05-30 08:08:14 -0600

    Product page

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -1,8 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Product.scss";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import BalanceIcon from "@mui/icons-material/Balance";
 
 const Product = () => {
-  return <div className="product">Product</div>;
+  const [selectedImg, setSelectedImg] = useState(0);
+  const [quantity, setQuantity] = useState(1);
+
+  const images = [
+    "https://polamarketing.com/wp-content/uploads/2024/06/On_Model-13@2x.jpg",
+    "https://www.shutterstock.com/image-photo/young-black-woman-cream-suit-600nw-2491218847.jpg",
+  ];
+
+  return (
+    <div className="product">
+      <div className="left">
+        <div className="images">
+          <img src={images[0]} alt="" onClick={(e) => setSelectedImg(0)} />
+          <img src={images[1]} alt="" onClick={(e) => setSelectedImg(1)} />
+        </div>
+        <div className="mainImg">
+          <img src={images[selectedImg]} alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <h1>Title</h1>
+        <span className="price">$199</span>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
+          voluptatum, asperiores sed adipisci libero expedita excepturi
+          architecto velit praesentium distinctio, unde, temporibus laborum
+          voluptate. Maiores quasi vel illo in eos!
+        </p>
+        <div className="quantity">
+          <button
+            onClick={() => setQuantity((prev) => (prev === 1 ? 1 : prev - 1))}
+          >
+            -
+          </button>
+          {quantity}
+          <button onClick={() => setQuantity((prev) => prev + 1)}>+</button>
+        </div>
+        <button className="add">
+          <AddShoppingCartIcon /> ADD TO CART
+        </button>
+        <div className="links">
+          <div className="item">
+            <FavoriteBorderIcon /> ADD TO WISH LIST
+          </div>
+          <div className="item">
+            <BalanceIcon /> ADD TO COMPARE
+          </div>
+        </div>
+        <div className="info">
+          <span>Vendor: Polo</span>
+          <span>Product Type: T-Shirt</span>
+          <span>Tag: T-Shirt, Women, Top</span>
+        </div>
+        <hr />
+        <div className="info">
+          <span>DESCRIPTION</span>
+          {/* cant get hr tag to work with flex so used div */}
+          <div className="hr"></div>
+          <span>ADDITIONAL INFORMATION</span>
+          <div className="hr"></div>
+          <span>FAQ</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
